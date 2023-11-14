20f9f9c10ee4429d81e4c573d793db402e998dff
amitaccio <142372667+amitaccio@users.noreply.github.com>
2023-11-14 18:23:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -6,13 +6,16 @@ function isSameType(value1, value2) {
 	else if(typeof value1 != typeof value){
 		return false;
 	}
+	else if(typeof value1==NaN || typeof value2==NaN){
+		return false;
+	}
 	else{
 		return true;
 	}
 }
 
-// do not change the code below.
-let value1 = prompt("Enter Start of the Range.");
-let value2 = prompt("Enter End Of the Range.");
+// do not change the code below.dd
+ let value1 = prompt("Enter Start of the Range.");
+ let value2 = prompt("Enter End Of the Range.");
 
 alert(isSameType(value1, value2));
